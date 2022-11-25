5d482e0b2c6579cee3d2a9486e08d9ffb072864a
Nosov Vitaly <vitalinosov2000@gmail.com>
2022-11-25 14:37:55 +0300

    Added adaptive version to header block

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,16 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
+const burger = document.querySelector('.burger')
+const headerTop = document.querySelector('.header__top')
+const body = document.querySelector('body')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    headerTop.classList.toggle('active')
+    body.classList.toggle('lock')
+})
+
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
