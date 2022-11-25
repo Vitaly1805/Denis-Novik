b931e2908a80e893fc14f252178b7b7b66733f1e
Nosov Vitaly <vitalinosov2000@gmail.com>
2022-11-25 17:51:35 +0300

    fixed to adaptive header. added anchors for menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,45 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
+//Переключение мобильного меню
+function toogleMenu() {
+    burger.classList.toggle('active')
+    headerTop.classList.toggle('active')
+    langs.classList.toggle('active')
+    body.classList.toggle('lock')
+}
+
 const burger = document.querySelector('.burger')
 const headerTop = document.querySelector('.header__top')
 const body = document.querySelector('body')
+const langs = document.querySelector('.denis__langs')
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    headerTop.classList.toggle('active')
-    body.classList.toggle('lock')
+    toogleMenu()
+})
+
+
+//Универсальный якорь для пунктов меню
+const menuItems = document.querySelectorAll('.menu__item')
+
+menuItems.forEach(menuItem => {
+    const blockName = menuItem.className.match(/menu__(?!item)(.*)/) ? menuItem.className.match(/menu__(?!item)(.*)/)[1] : null
+
+    if(blockName) {
+        const aboutBlock = document.querySelector('.' + blockName)
+
+        console.log(aboutBlock)
+
+        menuItem.addEventListener('click', (event) => {
+            event.preventDefault()
+
+            if(burger.classList.contains('active')) {
+                toogleMenu()
+            }
+
+            aboutBlock.scrollIntoView({alignTop: true, behavior: "smooth"})
+        })
+    }
 })
 
 /*
